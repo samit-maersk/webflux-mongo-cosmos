030dafd47a15db311d0ccc205c0c447ef228751d
samit-maersk <SamitKumar.Patel@maersk.com>
2023-03-14 15:44:39 +0100

    added tracing and logging

--- src/main/java/net/samitkumarpatel/webfluxmongocosmos/WebfluxMongoCosmosApplication.java
+++ src/main/java/net/samitkumarpatel/webfluxmongocosmos/WebfluxMongoCosmosApplication.java
@@ -1,5 +1,8 @@
 package net.samitkumarpatel.webfluxmongocosmos;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.ObservationTextPublisher;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,14 +11,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.observability.micrometer.Micrometer;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
 
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
@@ -28,15 +39,23 @@ public class WebfluxMongoCosmosApplication {
 		SpringApplication.run(WebfluxMongoCosmosApplication.class, args);
 	}
 
+	// print all the details in the log
+	@Bean
+	public ObservationTextPublisher printingObservationHandler() {
+		return new ObservationTextPublisher();
+	}
+
 	@Bean
 	public RouterFunction<ServerResponse> route(Handler handler) {
 		return RouterFunctions
 				.route()
-				.GET("/customer/all", handler::all)
-				.GET("/customer/{id}", handler::customerById )
-				.POST("/customer", handler::save)
-				.PUT("/customer/{id}", handler::update)
-				.DELETE("/customer/{id}", handler::delete)
+				.path("/customer", builder ->
+						builder
+								.GET("/all", handler::all)
+								.GET("/{id}", handler::customerById )
+								.POST("", handler::save)
+								.PUT("/{id}", handler::update)
+								.DELETE("/{id}", handler::delete))
 				.build();
 	}
 }
@@ -44,38 +63,97 @@ public class WebfluxMongoCosmosApplication {
 
 @Component
 @Slf4j
-record Handler(CustomerRepository customerRepository) {
+record Handler(CustomerService customerService) {
 	public Mono<ServerResponse> all(ServerRequest request) {
-		return ok().body(customerRepository.findAll().collectList(), List.class);
+		return ok().body(customerService.getAll().collectList(), List.class);
 	}
 
 	public Mono<ServerResponse> customerById(ServerRequest request) {
-		return ok().body(customerRepository.findById(request.pathVariable("id")), Customer.class);
+		var id = request.pathVariable("id");
+		return ok().body(customerService.getById(id), Customer.class);
 	}
 
 	public Mono<ServerResponse> save(ServerRequest request) {
 		return request
 				.bodyToMono(Customer.class)
-				.flatMap(customer -> ok().body(customerRepository.save(customer), Customer.class));
+				.flatMap(customer -> ok().body(customerService.add(customer), Customer.class));
 	}
 
 	public Mono<ServerResponse> update(ServerRequest request) {
 		var id = request.pathVariable("id");
-		return customerRepository
-				.findById(id)
-				.doOnNext(c -> log.info("db customer : {}",c))
-				.flatMap(customer -> {
-					return request
-							.bodyToMono(Customer.class)
-							.doOnNext(customer1 -> log.info("request customer : {}", customer1))
-							.map(customer1 -> Customer.builder().id(customer.id()).name(customer1.name()).age(customer1.age()).build())
-							.flatMap(customer2 -> ok().body(customerRepository.save(customer2), Customer.class));
-				});
+		return request
+					.bodyToMono(Customer.class)
+					.doOnNext(customer -> log.info("request customer : {}", customer))
+					.flatMap(customer -> ok().body(customerService.update(id, customer), Customer.class));
 	}
 
 	public Mono<ServerResponse> delete(ServerRequest request) {
 		var id = request.pathVariable("id");
-		return ok().body(customerRepository.deleteById(id), Void.class);
+		return ok().body(customerService.delete(id), Void.class);
+	}
+}
+
+interface CustomerService {
+	Flux<Customer> getAll();
+	Mono<Customer> getById(String id);
+	Mono<Customer> add(Customer customer);
+	Mono<Customer> update(String id, Customer customer);
+	Mono<Void> delete(String id);
+}
+@Service
+@Slf4j
+@AllArgsConstructor
+class CustomerServiceImpl implements CustomerService {
+	private final Supplier<Long> latency = () -> new Random().nextLong(500);
+	final CustomerRepository customerRepository;
+
+	final ObservationRegistry registry;
+
+
+	@Override
+	public Flux<Customer> getAll() {
+		Long lat = latency.get();
+		return customerRepository
+				.findAll()
+				.delayElements(Duration.ofMillis(lat))
+				.name("getAll.call")
+				.tag("latency", lat > 250 ? "high" : "low")
+				.tap(Micrometer.observation(registry));
+	}
+	@Override
+	public Mono<Customer> getById(String id) {
+		//TODO handle empty 200 scenario from findById
+		Long lat = latency.get();
+		return customerRepository
+				.findById(id)
+				.delayElement(Duration.ofMillis(lat));
+	}
+	@Override
+	public Mono<Customer> add(Customer customer) {
+		Long lat = latency.get();
+		return customerRepository
+				.save(customer)
+				.delayElement(Duration.ofMillis(lat));
+	}
+
+	@Override
+	public Mono<Customer> update(String id, Customer customer) {
+		Long lat = latency.get();
+		return customerRepository
+				.findById(id)
+				.onErrorResume(e -> Mono.error(e))
+				.doOnNext(customer1 -> log.info("update: db data for id:{} is : {}",id, customer1))
+				//TODO handle empty 200 scenario from findById
+				.flatMap(customer1 -> {
+					log.info("update: going to be update with : {}", customer);
+					var customerWithUpdateData = Customer.builder().id(customer1.id()).name(customer.name()).age(customer.age()).build();
+					return add(customerWithUpdateData);
+				})
+				.delayElement(Duration.ofMillis(lat));
+	}
+	@Override
+	public Mono<Void> delete(String id) {
+		return customerRepository.deleteById(id);
 	}
 }
 
@@ -88,4 +166,11 @@ record Customer(
 		int age) {
 	@Builder
 	public Customer {}
+}
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class DataNotFoundException extends RuntimeException {
+	public DataNotFoundException(String message) {
+		super(message);
+	}
 }
--- src/test/java/net/samitkumarpatel/webfluxmongocosmos/WebfluxMongoCosmosApplicationTests.java
+++ src/test/java/net/samitkumarpatel/webfluxmongocosmos/WebfluxMongoCosmosApplicationTests.java
@@ -1,22 +1,31 @@
 package net.samitkumarpatel.webfluxmongocosmos;
 
+import io.micrometer.observation.ObservationRegistry;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.configuration.IMockitoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
+
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Testcontainers
@@ -27,6 +36,7 @@ class WebfluxMongoCosmosApplicationTests {
 
 	@Autowired
 	CustomerRepository customerRepository;
+
 	@DynamicPropertySource
 	static void redisProperties(DynamicPropertyRegistry registry) {
 		registry.add("spring.data.mongodb.database", () -> "db");
@@ -38,6 +48,8 @@ class WebfluxMongoCosmosApplicationTests {
 	@AfterEach
 	void tearDown() {}
 
+	@MockBean
+	ObservationRegistry registry;
 	@Test
 	void contextLoads() {
 	}
@@ -57,6 +69,57 @@ class WebfluxMongoCosmosApplicationTests {
 
 	}
 
+	@Test
+	void serviceTest() {
+		CustomerRepository customerRepository = Mockito.mock(CustomerRepository.class);
+		CustomerService customerService = new CustomerServiceImpl(customerRepository, registry);
+		//static data
+		var customerOne = Customer.builder().id("1").name("One").age(30).build();
+		var customerTwo = Customer.builder().id("2").name("Two").age(35).build();
+		//mock
+		when(customerRepository.save(any(Customer.class))).thenReturn(Mono.just(customerOne));
+		when(customerRepository.findAll()).thenReturn(Flux.just(customerOne, customerTwo));
+		when(customerRepository.deleteById(anyString())).thenReturn(Mono.empty().then());
+		when(customerRepository.findById("3")).thenReturn(Mono.error(new DataNotFoundException("not found")));
+		when(customerRepository.findById("1")).thenReturn(Mono.just(customerOne));
 
+		customerService
+				.getById("1")
+				.as(StepVerifier::create)
+				.consumeNextWith(customer -> {
+					assertEquals(customerOne, customer);
+				})
+				.verifyComplete();
 
+		customerService
+				.add(Customer.builder().name("One").age(30).build())
+				.as(StepVerifier::create)
+				.consumeNextWith(customer -> {
+					assertNotNull(customer);
+					assertNotNull(customer.id());
+				})
+				.verifyComplete();
+
+		//update
+		var updateCustomer = Customer.builder().id("1").name("One-Update").age(35).build();
+		when(customerRepository.save(updateCustomer)).thenReturn(Mono.just(updateCustomer));
+		customerService
+				.update("1", Customer.builder().name("One-Update").age(35).build())
+				.as(StepVerifier::create)
+				.consumeNextWith(customer -> {
+					assertNotNull(customer);
+					assertEquals("1", customer.id());
+					assertEquals("One-Update", customer.name());
+					assertEquals(35, customer.age());
+				})
+				.verifyComplete();
+
+		//update 404
+		customerService
+				.update("3", Customer.builder().name("One-Update").age(35).build())
+				.as(StepVerifier::create)
+				.expectErrorMatches(e -> e instanceof DataNotFoundException)
+				.verify();
+
+	}
 }
